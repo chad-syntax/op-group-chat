dd906c5c25a1e4e609f2aab9c4c54f8b61997743
Chad $yntax <chad@chadsyntax.com>
2025-07-15 14:54:15 -0400

    prompt updates

--- start.ts
+++ start.ts
@@ -26,8 +26,8 @@ const completionLogDirTransformer = ({
     `${
       prompt.slug === 'one-piece-character-response-decision'
         ? 'decider'
-        : 'character'
-    }-${variables.name}`
+        : `character-${variables.name}`
+    }`
   );
   return dirName;
 };
@@ -41,19 +41,16 @@ const client = new AgentsmithClient<Agency>(apiKey, projectId, {
 type ThreadMessage = {
   name: string;
   message: string;
+  turn: number;
 };
 
 type CharacterResponseDecision = {
+  character: string;
+  score: number;
   reasons_to_respond: string[];
   reasons_not_to_respond: string[];
-  score: number;
 };
-
 const thread: ThreadMessage[] = [];
-
-// const luffy = await client.getPrompt('luffy');
-// const nami = await client.getPrompt('nami');
-// const usopp = await client.getPrompt('usopp');
 
 const characters = [
   'Luffy',
@@ -73,11 +70,13 @@ const deciderPrompt = await client.getPrompt(
 );
 const prompt = await client.getPrompt('op-character');
 
-// let forceConversationNum = 0;
+const startingScenario =
+  'The Straw Hat Pirates are on the sunny on their way to the island of Elbaph. They are talking about the island and how they will get there. Luffy is impatient and wants to get there faster.';
+
 let turnsWithoutResponses = 0;
 
-while (i < 20) {
-  console.log(`TURN ${i}`, `turnsWithoutResponses: ${turnsWithoutResponses}`);
+while (i < 40) {
+  // console.log(`TURN ${i}`, `turnsWithoutResponses: ${turnsWithoutResponses}`);
   let characterResponseDecisions: { character: string; score: number }[] = [];
   try {
     const { content } = await deciderPrompt.execute({
@@ -85,23 +84,13 @@ while (i < 20) {
       turnsWithoutResponses,
       characters,
     });
-    let parsedContent: {
-      character: string;
-      score: number;
-      reasons_to_respond: string[];
-      reasons_not_to_respond: string[];
-    }[] = JSON.parse(content ?? '[]');
-    characterResponseDecisions = parsedContent.map(
-      (d: {
-        character: string;
-        score: number;
-        reasons_to_respond: string[];
-        reasons_not_to_respond: string[];
-      }) => ({
-        character: d.character,
-        score: d.score + turnsWithoutResponses * Math.random() * 2,
-      })
+    let parsedContent: CharacterResponseDecision[] = JSON.parse(
+      content ?? '[]'
     );
+    characterResponseDecisions = parsedContent.map((d) => ({
+      character: d.character,
+      score: d.score + Math.round(turnsWithoutResponses * Math.random() * 2),
+    }));
   } catch (e) {
     console.error('Error executing deciderPrompt', e);
     characterResponseDecisions = characters.map((c: string) => ({
@@ -109,63 +98,82 @@ while (i < 20) {
       score: 0,
     }));
   }
-  console.log(
-    'decisions: ',
-    characterResponseDecisions
-      .map(
-        (d: { character: string; score: number }) =>
-          `(${d.character}: ${d.score})`
-      )
-      .join(' ')
-  );
+
   const charactersThatShouldRespond = characterResponseDecisions
-    .filter((c: { character: string; score: number }) => c.score >= 5)
-    .map((c: { character: string; score: number }) => c.character);
+    .filter((c) => c.score >= 5)
+    .sort((a, b) => b.score - a.score)
+    .map((c) => c.character);
+
   const characterResponses = await Promise.all(
     charactersThatShouldRespond.map(async (character: string) => {
-      const { content } = await prompt.execute({
-        thread,
-        name: character,
-      });
-      return {
-        character,
-        content,
-      };
+      try {
+        const { tokens } = await prompt.execute(
+          {
+            thread,
+            name: character,
+            startingScenario,
+          },
+          {
+            config: {
+              stream: true,
+            },
+          }
+        );
+        return {
+          character,
+          tokens,
+        };
+      } catch (e) {
+        console.error(`Error executing prompt for character ${character}:`, e);
+        return {
+          character,
+          content: null,
+        };
+      }
     })
   );
-  // console.log('characterResponses', characterResponses);
 
-  const actualResponses = characterResponses.filter(
-    (r) =>
-      r.content !== null &&
-      r.content.trim() !== '' &&
-      !r.content.includes('NO_RESPONSE')
-  );
-
-  if (actualResponses.length === 0) {
+  if (characterResponses.length === 0) {
     turnsWithoutResponses++;
   } else {
     turnsWithoutResponses = 0;
   }
 
+  const structuredResponses: { character: string; content: string }[] = [];
+
+  for (const r of characterResponses) {
+    if (!r.tokens) {
+      continue;
+    }
+    process.stdout.write(`${r.character}: `);
+    let characterResponse = '';
+
+    for await (const token of r.tokens) {
+      await new Promise((resolve) => setTimeout(resolve, 100));
+      characterResponse += token ?? '';
+      process.stdout.write(token ?? '');
+    }
+
+    process.stdout.write('\n');
+    structuredResponses.push({
+      character: r.character,
+      content: characterResponse,
+    });
+  }
+
   thread.push(
-    ...actualResponses.map((r) => ({
+    ...structuredResponses.map((r) => ({
       name: r.character,
-      message: r.content!,
+      message: r.content,
+      turn: i,
     }))
   );
 
-  for (const r of actualResponses) {
-    console.log(`${r.character}: ${r.content}`);
-  }
-
-  // console.log('updated thread', thread);
-
   i++;
 }
-
+console.log('Starting Scenario:', startingScenario);
 console.log(
-  'final thread:',
-  thread.map((t) => `${t.name}: ${t.message}`).join('\n')
+  'Final Thread:\n',
+  thread.map((t) => `[${t.turn}] ${t.name}: ${t.message}`).join('\n')
 );
 await client.shutdown();
